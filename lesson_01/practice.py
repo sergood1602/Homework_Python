260cef8cb09451dff17ea96c8b28dd63811b7d16
sergood1602 <sergood1602@gmail.com>
2026-03-22 16:41:45 +0300

    first commit

---
+++ lesson_01/practice.py
@@ -0,0 +1,31 @@
+my_heigh = 172
+print(my_heigh)
+
+
+my_name = "Сергей"
+my_name = "Сергей Костромин"
+print(my_name)
+
+
+pet_name = input("Как зовут вашего питомца? ")
+print("Ваш любимчик - " + pet_name)
+
+
+def print_python():
+    print("Учу Python!")
+
+
+print_python()
+
+
+def print_letter(let):
+    print(let, end='')
+
+
+print_letter('С')
+print_letter('т')
+print_letter('у')
+print_letter('д')
+print_letter('е')
+print_letter('н')
+print_letter('т')
